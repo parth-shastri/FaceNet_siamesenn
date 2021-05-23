6f19284e6e88cd01b11a1ca49b3442cab09baa62
parth-shastri <shastriparth28@gmail.com>
2021-05-23 12:19:30 +0530

    Commit

---
+++ config.py
@@ -0,0 +1,10 @@
+IMAGE_SHAPE = (200, 200)
+BATCH_SIZE = 32
+CKPT_DIR = "face_net-ckpt/model_ckpt"
+
+ANCHOR_DIR = "left/left"
+POS_DIR = "right/right"
+
+EMBED_DIM = 256
+MARGIN = 0.5
+ARCHITECTURE_PATH = "model_architecture.json"
